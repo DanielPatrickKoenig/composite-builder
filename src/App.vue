702d45713fc72d20c692a8eb152a2a07a75bd626
Dan Koenig <dkoenig@gongos.com>
2018-12-19 09:37:38 -0500

    Signed-off-by: Dan Koenig <dkoenig@gongos.com>

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,19 @@
   <div>
     <div class="viewports">
       <div :style="getQuatStyle(false, false)">
-        <threerender class="prespectiv-view" v-if="shouldRender" :objects="objectManifest" :perspective='75'></threerender>
+        <threerender class="prespectiv-view" v-if="shouldRender" :objects="objectManifest" :perspective='75' :signature="signature"></threerender>
       </div>
       <div :style="getQuatStyle(true, true)">
         <!-- <wireframe :objects="objectManifest" :axis="AxisTypes.X" v-if="shouldRender"></wireframe> -->
-        <threerender class="x-view" v-if="shouldRender" :objects="objectManifest" :perspective='0' :axis="AxisTypes.X"></threerender>
+        <threerender class="x-view" v-if="shouldRender" :objects="objectManifest" :perspective='0' :axis="AxisTypes.X" :signature="signature"></threerender>
       </div>
       <div :style="getQuatStyle(false, true)">
         <!-- <wireframe :objects="objectManifest" :axis="AxisTypes.Y" v-if="shouldRender"></wireframe> -->
-        <threerender class="y-view" v-if="shouldRender" :objects="objectManifest" :perspective='0' :axis="AxisTypes.Y"></threerender>
+        <threerender class="y-view" v-if="shouldRender" :objects="objectManifest" :perspective='0' :axis="AxisTypes.Y" :signature="signature"></threerender>
       </div>
       <div :style="getQuatStyle(true, false)">
         <!-- <wireframe :objects="objectManifest" :axis="AxisTypes.Z" v-if="shouldRender"></wireframe> -->
-        <threerender class="z-view" v-if="shouldRender" :objects="objectManifest" :perspective='0' :axis="AxisTypes.Z"></threerender>
+        <threerender class="z-view" v-if="shouldRender" :objects="objectManifest" :perspective='0' :axis="AxisTypes.Z" :signature="signature"></threerender>
       </div>
     </div>
     
@@ -22,6 +22,7 @@
 </template>
 
 <script>
+import {EventBus} from './components/EventBus.js'
 import ThreeRendering from './components/ThreeRendering.vue'
 import {ObjectTypes} from './components/ObjectTypes.js'
 import {AxisTypes} from './components/AxisTypes.js'
@@ -42,7 +43,8 @@ export default {
         {type: ObjectTypes.SPHERE, position: {x: 3, y: 2, z: -11}, rotation: {x: 0, y: -40, z: 0}, size: {radius: 3}}
       ],
       centers: {x: 50, y: 50},
-      canRenderViewports: true
+      canRenderViewports: true,
+      signature: 0
     }
   },
   computed: {
@@ -72,6 +74,30 @@ export default {
     var self = this
     window.addEventListener('resize', function () {
       self.rerenderViewports()
+    })
+    EventBus.$on('wireframe-drag-element', (n) => {
+      console.log(n)
+      self.$data.signature += 1
+      if (self.$data.signature > 20) {
+        self.$data.signature = 0
+      }
+      switch (n.axis) {
+        case AxisTypes.X: {
+          self.$data.objectManifest[n.index].position.z = n.x / n.scale
+          self.$data.objectManifest[n.index].position.y = n.y / n.scale
+          break
+        }
+        case AxisTypes.Y: {
+          self.$data.objectManifest[n.index].position.x = n.x / n.scale
+          self.$data.objectManifest[n.index].position.z = n.y / n.scale
+          break
+        }
+        case AxisTypes.Z: {
+          self.$data.objectManifest[n.index].position.x = n.x / n.scale
+          self.$data.objectManifest[n.index].position.y = n.y / n.scale
+          break
+        }
+      }
     })
   }
 }
--- src/components/SingleAxisWireFrameElement.vue
+++ src/components/SingleAxisWireFrameElement.vue
@@ -1,17 +1,41 @@
 <template>
-  <div class="centered-element" v-if="proxies.x != null && proxies.x != undefined">
-    <a :style="'left:'+((object.position[proxies.x]*scale)).toString()+'px;top:'+((object.position[proxies.y]*scale)).toString()+'px;width:'+moverSize.width.toString()+'px;height:'+moverSize.height.toString()+'px;margin-left:'+(moverSize.width/-2).toString()+'px;margin-top:'+(moverSize.height/-2).toString()+'px;'"></a>
+  <div :id="containerID" class="centered-element" v-if="proxies.x != null && proxies.x != undefined">
+    <a v-on:mousedown="onDown" :style="'left:'+((object.position[proxies.x]*scale)).toString()+'px;top:'+((object.position[proxies.y]*scale)).toString()+'px;width:'+moverSize.width.toString()+'px;height:'+moverSize.height.toString()+'px;margin-left:'+(moverSize.width/-2).toString()+'px;margin-top:'+(moverSize.height/-2).toString()+'px;'"></a>
+    <div v-if="dragging" v-on:mousemove="onMove" v-on:mouseup="onUp" class="element-drag-layer"></div>
   </div>
 </template>
 <script>
+import {EventBus} from './EventBus.js'
 import {AxisTypes} from './AxisTypes.js'
 export default {
-  props: ['axis', 'object', 'offset', 'scale'],
+  props: ['axis', 'object', 'offset', 'scale', 'index'],
   data () {
     return {
+      containerID: 'elementWireFrameID_' + Math.random().toString().split('.').join('') + Math.random().toString().split('.').join('') + Math.random().toString().split('.').join(''),
       AxisTypes: AxisTypes,
       proxies: {},
-      moverSize: {width: 12, height: 12}
+      moverSize: {width: 12, height: 12},
+      dragging: false
+    }
+  },
+  methods: {
+    onDown: function (e) {
+      var self = this
+      self.$data.dragging = true
+    },
+    onMove: function (e) {
+      var self = this
+      EventBus.$emit('wireframe-drag-element', {
+        axis: self.axis,
+        index: self.index,
+        x: e.pageX - document.getElementById(self.$data.containerID).getBoundingClientRect().left,
+        y: e.pageY - document.getElementById(self.$data.containerID).getBoundingClientRect().top,
+        scale: self.scale
+      })
+    },
+    onUp: function (e) {
+      var self = this
+      self.$data.dragging = false
     }
   },
   mounted: function () {
@@ -45,7 +69,16 @@ div.centered-element{
     background-color: #000000;
     border-radius: 200px;
   }
+  > div.element-drag-layer{
+    position: fixed;
+    top:0;
+    left:0;
+    right:0;
+    bottom:0;
+    z-index:400;
+  }
 }
+
 </style>
 
 
